bcf7de68574404c4bccb1abbba78678381cf7ab9
Caio <caiozs21@gmail.com>
2023-08-18 12:52:09 -0300

    feat: get tweets without pagination

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Tweet } from './entities/tweet.entity';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AppService {
+  private tweets: Tweet[];
+  private users: User[];
+
+  constructor() {
+    this.users = [];
+    this.users.push(new User('Usuario 1', 'foto de perfil1'));
+    this.tweets = [];
+    this.tweets.push(
+      new Tweet(new User('Usuario 1', 'foto de perfil1'), 'Primeiro Tweet'),
+    );
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getTweets(): Tweet[] {
+    return this.tweets;
   }
 }
